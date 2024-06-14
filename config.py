72b163bdee85939b0382dc159314009075431ef0
Goody-Code <85070172+Goody-Code@users.noreply.github.com>
2024-06-14 10:48:32 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,4 +3,5 @@ TELEGRAM_API_TOKEN = '7150721411:AAFRM5keUChtTDJgTGlyeYG6qTrKwHOWjnA'
 AZURE_COMPUTER_VISION_ENDPOINT = 'https://imagesbotsolver.cognitiveservices.azure.com/'
 AZURE_COMPUTER_VISION_SUBSCRIPTION_KEY = '2444bfedecf34964abbfed9c7112e7ff'
 WEBHOOK_URL = 'https://image-solver.onrender.com/'  # أضف عنوان الويب الخاص بالخادم
-
+HUGGINGFACE_TOKEN = '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'
+HUGGING_FACE_API_ENDPOINT = "https://api-inference.huggingface.co/models/"
