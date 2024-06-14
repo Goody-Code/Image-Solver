596086c4d4811d5e075c5cf80e5a441f95287022
Goody-Code <85070172+Goody-Code@users.noreply.github.com>
2024-06-14 07:09:08 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,4 +2,4 @@
 TELEGRAM_API_TOKEN = '7150721411:AAFRM5keUChtTDJgTGlyeYG6qTrKwHOWjnA'
 AZURE_COMPUTER_VISION_ENDPOINT = 'https://imagesbotsolver.cognitiveservices.azure.com/'
 AZURE_COMPUTER_VISION_SUBSCRIPTION_KEY = '2444bfedecf34964abbfed9c7112e7ff'
-WEBHOOK_URL = 'https://image-solver.onrender.com'  # أضف عنوان الويب الخاص بالخادم
+
