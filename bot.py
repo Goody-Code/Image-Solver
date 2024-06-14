61230cec0f330361485dd0b75fbd184ff515f0e9
Goody-Code <85070172+Goody-Code@users.noreply.github.com>
2024-06-14 04:41:42 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,78 +1,39 @@
-import logging
-from io import BytesIO
-from PIL import Image
-import telepot
-from telepot.loop import MessageLoop
-from telepot.namedtuple import InlineKeyboardMarkup, InlineKeyboardButton
-from transformers import GPTNeoForCausalLM, AutoTokenizer
-from azure.cognitiveservices.vision.computervision import ComputerVisionClient
-from azure.cognitiveservices.vision.computervision.models import OperationStatusCodes
-from msrest.authentication import CognitiveServicesCredentials
+import os
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+from image_processing import extract_text_from_image
+from nlp_model import solve_question
+from config import TELEGRAM_API_TOKEN, WEBHOOK_URL
 
-# إعداد التسجيلات
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+def start(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text('أرسل صورة السؤال الذي تريد حله.')
 
-# إعداد Azure OCR
-subscription_key = '2444bfedecf34964abbfed9c7112e7ff'
-endpoint = 'https://imagesbotsolver.cognitiveservices.azure.com/'
+def handle_image(update: Update, context: CallbackContext) -> None:
+    photo = update.message.photo[-1].get_file()
+    file_path = f'images/{photo.file_id}.jpg'
+    photo.download(file_path)
+    
+    extracted_text = extract_text_from_image(file_path)
+    answer = solve_question(extracted_text)
+    
+    update.message.reply_text(f'السؤال: {extracted_text}\n\nالحل: {answer}')
+    
+    os.remove(file_path)  # حذف الصورة بعد المعالجة
 
-computervision_client = ComputerVisionClient(endpoint, CognitiveServicesCredentials(subscription_key))
+def main():
+    updater = Updater(TELEGRAM_API_TOKEN, use_context=True)
+    dispatcher = updater.dispatcher
+    
+    dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(MessageHandler(Filters.photo, handle_image))
+    
+    # إعداد webhook
+    updater.start_webhook(listen="0.0.0.0",
+                          port=int(os.environ.get('PORT', 5000)),
+                          url_path=TELEGRAM_API_TOKEN)
+    updater.bot.set_webhook(WEBHOOK_URL + TELEGRAM_API_TOKEN)
+    
+    updater.idle()
 
-# تحميل النموذج والمحول
-model_name = "EleutherAI/gpt-neo-1.3B"
-tokenizer = AutoTokenizer.from_pretrained(model_name)
-model = GPTNeoForCausalLM.from_pretrained(model_name)
-
-def handle(msg):
-    content_type, chat_type, chat_id = telepot.glance(msg)
-
-    if content_type == 'photo':
-        # استقبال الصورة
-        file_id = msg['photo'][-1]['file_id']
-        file_path = bot.getFile(file_id)['file_path']
-        image_url = f"https://api.telegram.org/file/bot{API_TOKEN}/{file_path}"
-
-        # تحميل الصورة ومعالجتها
-        image_data = requests.get(image_url).content
-        image = Image.open(BytesIO(image_data))
-
-        # استخدام Azure OCR لاستخراج النص من الصورة
-        image_bytes = BytesIO()
-        image.save(image_bytes, format='JPEG')
-        image_bytes = image_bytes.getvalue()
-
-        ocr_results = computervision_client.read_in_stream(BytesIO(image_bytes), raw=True)
-        operation_location = ocr_results.headers["Operation-Location"]
-        operation_id = operation_location.split("/")[-1]
-
-        # انتظار النتائج
-        while True:
-            result = computervision_client.get_read_result(operation_id)
-            if result.status not in ['notStarted', 'running']:
-                break
-
-        if result.status == OperationStatusCodes.succeeded:
-            text = ""
-            for text_result in result.analyze_result.read_results:
-                for line in text_result.lines:
-                    text += line.text + " "
-        else:
-            bot.sendMessage(chat_id, 'حدث خطأ أثناء قراءة النص من الصورة.')
-            return
-
-        # استخدام النموذج للإجابة على الأسئلة
-        inputs = tokenizer.encode(text + tokenizer.eos_token, return_tensors='pt')
-        reply_ids = model.generate(inputs, max_length=1000, pad_token_id=tokenizer.eos_token_id)
-        reply = tokenizer.decode(reply_ids[:, inputs.shape[-1]:][0], skip_special_tokens=True)
-
-        bot.sendMessage(chat_id, reply)
-
-# تكوين البوت
-API_TOKEN = '7150721411:AAFRM5keUChtTDJgTGlyeYG6qTrKwHOWjnA'
-bot = telepot.Bot(API_TOKEN)
-MessageLoop(bot, handle).run_as_thread()
-
-print('Listening ...')
-
-while True:
-    pass
+if __name__ == '__main__':
+    main()
