ce90d509137b42e7ef9b4fc8ab873f07ccbef59d
Goody-Code <85070172+Goody-Code@users.noreply.github.com>
2024-06-14 04:55:53 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+# إعدادات المشروع
+TELEGRAM_API_TOKEN = '7150721411:AAFRM5keUChtTDJgTGlyeYG6qTrKwHOWjnA'
+AZURE_COMPUTER_VISION_ENDPOINT = 'https://imagesbotsolver.cognitiveservices.azure.com/'
+AZURE_COMPUTER_VISION_SUBSCRIPTION_KEY = '2444bfedecf34964abbfed9c7112e7ff'
+WEBHOOK_URL = 'https://photosolver.onrender.com/'  # أضف عنوان الويب الخاص بالخادم
