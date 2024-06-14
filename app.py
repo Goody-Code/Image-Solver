7d7e762f53ae9c5fdd91c6800ad0d7924bd9fd45
Goody-Code <85070172+Goody-Code@users.noreply.github.com>
2024-06-14 10:31:05 +0300

    Update app.py

--- app.py
+++ app.py
@@ -36,4 +36,6 @@ def webhook():
     return "ok"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
+    from os import environ
+    port = int(environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
