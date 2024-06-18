6ce970ed2d580ffd7843e0e493d1880067138a7a
Goody-Code <85070172+Goody-Code@users.noreply.github.com>
2024-06-19 02:02:51 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
-# إعدادات المشروع
-TELEGRAM_API_TOKEN = '7150721411:AAFRM5keUChtTDJgTGlyeYG6qTrKwHOWjnA'
-AZURE_COMPUTER_VISION_ENDPOINT = 'https://imagesbotsolver.cognitiveservices.azure.com/'
-AZURE_COMPUTER_VISION_SUBSCRIPTION_KEY = '2444bfedecf34964abbfed9c7112e7ff'
-WEBHOOK_URL = 'https://image-solver.onrender.com/'  # أضف عنوان الويب الخاص بالخادم
-HUGGINGFACE_TOKEN = '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'
-HUGGING_FACE_API_ENDPOINT = "https://api-inference.huggingface.co/models/"
+import os
+
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN', '7150721411:AAFRM5keUChtTDJgTGlyeYG6qTrKwHOWjnA')
+AZURE_SUBSCRIPTION_KEY = os.getenv('AZURE_SUBSCRIPTION_KEY', '2444bfedecf34964abbfed9c7112e7ff')
+AZURE_ENDPOINT = os.getenv('AZURE_ENDPOINT', 'https://imagesbotsolver.cognitiveservices.azure.com/')
+HUGGINGFACE_TOKEN = os.getenv('HUGGINGFACE_TOKEN', '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')
+WEBHOOK_URL = os.getenv('WEBHOOK_URL', 'https://image-solver.onrender.com/')
+HUGGING_FACE_API_ENDPOINT = os.getenv('HUGGING_FACE_API_ENDPOINT', 'https://api-inference.huggingface.co/models/')
