2017502522134f7521d0aef4c77821c5024dfa61
Goody-Code <85070172+Goody-Code@users.noreply.github.com>
2024-06-19 02:03:51 +0300

    Update nlp_model.py

--- nlp_model.py
+++ nlp_model.py
@@ -1,10 +1,15 @@
 import os
 from transformers import pipeline
-
-token = os.getenv("HUGGINGFACE_TOKEN")
+from config import HUGGINGFACE_TOKEN, HUGGING_FACE_API_ENDPOINT
 
 # تهيئة الأنابيب للإجابة على الأسئلة
-qa_pipeline = pipeline("question-answering", model="EleutherAI/gpt-neo-1.3B", tokenizer="EleutherAI/gpt-neo-1.3B", use_auth_token=token)
+qa_pipeline = pipeline(
+    "question-answering", 
+    model="EleutherAI/gpt-neo-1.3B", 
+    tokenizer="EleutherAI/gpt-neo-1.3B", 
+    use_auth_token=HUGGINGFACE_TOKEN,
+    api_endpoint=HUGGING_FACE_API_ENDPOINT
+)
 
 def generate_response(cleaned_text: str) -> str:
     questions = re.split(r'\d+\.', cleaned_text)
