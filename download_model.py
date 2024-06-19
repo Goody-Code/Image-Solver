21169965a7f30bfae9ada362def483b68f42b240
Goody-Code <85070172+Goody-Code@users.noreply.github.com>
2024-06-19 04:50:35 +0300

    Update download_model.py

--- download_model.py
+++ download_model.py
@@ -1,5 +1,7 @@
-# download_model.py
 from transformers import pipeline
 
 # تحديد النموذج الذي نريد تحميله
 model = pipeline('question-answering', model='distilbert-base-uncased-distilled-squad')
+
+# يمكن تخزين النموذج في ذاكرة التخزين المؤقت
+model.save_pretrained('/app/model')
